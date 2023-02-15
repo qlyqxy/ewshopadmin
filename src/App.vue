a0ddfd2fe4755377fbd4cd8ab8d22ae5e6f070a4
lmonkey <lmonkey@lmonkey>
2023-02-15 16:48:10 +0800

    引入Naive UI,配置组件自动导入

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,3 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
 
 <template>
   <div>
@@ -11,8 +8,14 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <div>
+    <NButton>点我一下</NButton>
+  </div>
 </template>
+
+<script setup lang="ts">
+
+</script>
 
 <style scoped>
 .logo {
